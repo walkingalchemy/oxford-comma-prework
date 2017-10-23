1913ea2cf852d94c3d2247b76c820c9ad96f271a
James Royer <walkingalchemy@gmail.com>
2017-10-23 15:15:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length <= 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")
 end
